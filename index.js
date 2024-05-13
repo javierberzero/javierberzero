74892f79b2cfeec8804fcee9822d4b3708606493
Javier Mugnani <javieralejandroberzero@gmail.com>
2024-05-13 16:55:48 -0300

    Los dados funcionan correctamente

---
+++ index.js
@@ -0,0 +1,30 @@
+function generarNumero(){
+    
+    const numeroAleatorio = Math.floor(Math.random() * 3) + 1;
+          document.getElementById('randomNumber').innerHTML = `El número aleatorio es: ${numeroAleatorio}`;
+        }
+  
+        function generarNumero1(){
+    
+          const numeroAleatorio1 = Math.floor(Math.random() * 6) + 1;
+          document.getElementById('randomNumber1').innerHTML = `El número aleatorio es: ${numeroAleatorio1}`;
+        }
+        function generarNumero2(){
+    const numeroAleatorio2 = Math.floor(Math.random() * 8) + 1;
+  
+          document.getElementById('randomNumber2').innerHTML = `El número aleatorio es: ${numeroAleatorio2}`;
+        }
+        function generarNumero3(){
+    const numeroAleatorio3 = Math.floor(Math.random() * 10) + 1;
+  
+          document.getElementById('randomNumber3').innerHTML = `El número aleatorio es: ${numeroAleatorio3}`;
+        }     function generarNumero4(){
+    const numeroAleatorio4 = Math.floor(Math.random() * 20) + 1;
+  
+          document.getElementById('randomNumber4').innerHTML = `El número aleatorio es: ${numeroAleatorio4}`;
+        }
+        function generarNumero5(){
+          const numeroAleatorio5 = Math.floor(Math.random() * 100) + 1;
+        
+                document.getElementById('randomNumber5').innerHTML = `El número aleatorio es: ${numeroAleatorio5}`;
+              }
